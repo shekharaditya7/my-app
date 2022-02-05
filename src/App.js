e29446f43449842d948f8425e9dcc5e6d0400b19
adityaShekhar <shekharaditya7@gmail.com>
2022-02-05 09:48:12 +0530

    drawLine helper

--- src/App.js
+++ src/App.js
@@ -25,20 +25,20 @@ function App() {
     ctx.stroke();
   }
 
-  // function drawLine(ctx, begin, end, stroke = "black", width = 1) {
-  //   if (stroke) {
-  //     ctx.strokeStyle = stroke;
-  //   }
+  function drawLine(ctx, begin, end, stroke = "black", width = 1) {
+    if (stroke) {
+      ctx.strokeStyle = stroke;
+    }
 
-  //   if (width) {
-  //     ctx.lineWidth = width;
-  //   }
+    if (width) {
+      ctx.lineWidth = width;
+    }
 
-  //   ctx.beginPath();
-  //   ctx.moveTo(...begin);
-  //   ctx.lineTo(...end);
-  //   ctx.stroke();
-  // }
+    ctx.beginPath();
+    ctx.moveTo(...begin);
+    ctx.lineTo(...end);
+    ctx.stroke();
+  }
 
 
   function handleMouseMove(event){
