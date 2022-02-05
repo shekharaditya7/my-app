258af556b24ee13c4165f2f90deadc4c8efcd8c5
adityaShekhar <aditya.s@toppr.com>
2022-02-05 09:46:45 +0530

    basic canvas js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,46 @@ import { useRef } from 'react';
 
 function App() {
   const ref = useRef(null);
+  const can = useRef(null);
+
+  function draw() {
+    const canvas = document.querySelector("#canvas");
+
+    if (!canvas.getContext) {
+      return;
+    }
+    const ctx = canvas.getContext("2d");
+
+    // set line stroke and line width
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 1;
+
+    // draw a red line
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+    ctx.stroke();
+  }
+
+  // function drawLine(ctx, begin, end, stroke = "black", width = 1) {
+  //   if (stroke) {
+  //     ctx.strokeStyle = stroke;
+  //   }
+
+  //   if (width) {
+  //     ctx.lineWidth = width;
+  //   }
+
+  //   ctx.beginPath();
+  //   ctx.moveTo(...begin);
+  //   ctx.lineTo(...end);
+  //   ctx.stroke();
+  // }
+
 
   function handleMouseMove(event){
     console.log(event);
+    draw();
   }
 
   function handleDrag(event){
@@ -21,7 +58,8 @@ function App() {
   return (
     <div className="App">
         <img src={logo} className="App-logo" alt="logo" height={40} width={40}/>
-        <div ref={ref} id='drawer' onMouseDown={handleDrag} onMouseUp={handleLeave}>   
+        <div ref={ref} id='drawer' onMouseDown={handleDrag} onMouseUp={handleLeave}>
+           <canvas ref={can} id="canvas"></canvas>   
         </div>
     </div>
   );
