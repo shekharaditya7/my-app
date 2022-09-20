3fdbcac39b49a806a5f5157f10890813efce396e
Aditya Shekhar <aditya.shekhar9@byjus.com>
2022-09-20 15:20:02 +0530

    added base case

--- src/components/YTDeepLink.js
+++ src/components/YTDeepLink.js
@@ -9,17 +9,21 @@ export default function YTDeepLink() {
     const OS = getOperatingSystem();
     const { isMobileViewUtil } = getIsMobileView();
 
-    if (!videoId) return;
-
     if (!isMobileViewUtil)
-      window.location = `https://www.youtube.com/watch?v=${videoId}`;
+      window.location = `https://www.youtube.com/watch?v=${
+        videoId || "yeuYiShtmRg"
+      }`;
 
     switch (OS) {
       case ANDROID:
-        window.location = `intent://www.youtube.com/watch?v=${videoId}/#Intent;package=com.google.android.youtube;scheme=https;end`;
+        window.location = `intent://www.youtube.com/watch?v=${
+          videoId || "yeuYiShtmRg"
+        }/#Intent;package=com.google.android.youtube;scheme=https;end`;
         break;
       case IOS:
-        window.location = `vnd.youtube://www.youtube.com/watch?v=${videoId}`;
+        window.location = `vnd.youtube://www.youtube.com/watch?v=${
+          videoId || "yeuYiShtmRg"
+        }`;
         break;
       default:
         break;
