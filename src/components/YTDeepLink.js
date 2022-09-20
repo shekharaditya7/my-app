bd47ef95fe8497a2ae22200721bb2b00414fa2ea
Aditya Shekhar <aditya.shekhar9@byjus.com>
2022-09-20 11:16:39 +0530

    fixed android link

--- src/components/YTDeepLink.js
+++ src/components/YTDeepLink.js
@@ -9,7 +9,7 @@ export default function YTDeepLink() {
     if (!videoId) return;
     switch (OS) {
       case ANDROID:
-        window.location = `intent://www.youtube.com/watch?v=${videoId}`;
+        window.location = `intent://www.youtube.com/watch?v=${videoId}/#Intent;package=com.google.android.youtube;scheme=https;end`;
         break;
       case IOS:
         window.location = `vnd.youtube://www.youtube.com/watch?v=${videoId}`;
