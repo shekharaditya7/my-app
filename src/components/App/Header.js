cf6704d50e984f2d73870ceb4937a7f7a696af97
Aditya Shekhar <aditya.shekhar9@byjus.com>
2022-10-31 23:34:09 +0530

    pathname based title

--- src/components/App/Header.js
+++ src/components/App/Header.js
@@ -1,9 +1,21 @@
+import { BrowserRouter, useLocation } from "react-router-dom";
+import NAV_ITEMS from "./sidebar.constants";
 import styles from "./Header.module.scss";
 
 export default function Header({ onMenuIconClick }) {
+  const { pathname } = useLocation();
+  const getActivePathTitle = () => {
+    let title = "Home";
+    NAV_ITEMS.forEach(({ url, label }) => {
+      if (url === pathname) title = label;
+    });
+    return title;
+  };
+
   return (
     <div className={styles.wrapper}>
-      <span>Playground </span>
+      <span>{getActivePathTitle()} </span>
+      <div className={styles.label}>Frontend Playground</div>
       <img
         className={styles.profileIcon}
         src="/icon192.jpeg"
