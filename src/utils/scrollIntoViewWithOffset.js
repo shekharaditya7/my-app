d7a5b3fd072048639c968ca1870674869598b234
Aditya Shekhar <aditya.shekhar9@byjus.com>
2022-11-26 01:02:07 +0530

    added customScroll Into View

--- src/utils/scrollIntoViewWithOffset.js
+++ src/utils/scrollIntoViewWithOffset.js
@@ -1,3 +1,16 @@
-export default function scrollIntoViewWithOffset(offset) {
-    
+export default function scrollIntoViewWithOffset(offset = 0) {
+    const element = this;
+    if (!element instanceof HTMLElement){
+        alert('Invalid element')
+        return;
+    }
+    if(!element.isConnected){
+        alert('Not an DOM HTML element')
+    }
+    const { top } = element.getBoundingClientRect() || {};
+    window.scrollBy({
+        top: top - offset,
+        left: 0,
+        behavior : 'smooth'
+    })
 }
