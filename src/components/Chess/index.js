5a8032753e6371d99344c6d690fdb7c8fe0d414c
Aditya Shekhar <aditya.shekhar9@byjus.com>
2022-11-30 13:27:25 +0530

    instructions enhanced

--- src/components/Chess/index.js
+++ src/components/Chess/index.js
@@ -37,7 +37,7 @@ export default function Chess() {
     }
 
     if (turn.current !== piece?.color) {
-      setShowInstructions(true);
+      if (!!piece) setShowInstructions(true);
       return;
     }
 
