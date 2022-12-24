c7f659690483d124f596ee8aff644aeb1e7c00af
Aditya Shekhar <aditya.shekhar9@byjus.com>
2022-12-24 17:29:37 +0530

    worker version change

--- public/worker.js
+++ public/worker.js
@@ -1,45 +1,39 @@
 /* eslint-disable no-restricted-globals */
-const CACHE_NAME = "pwa-v4";
+const CACHE_NAME = "pwa-v5";
 
-const urlsToCache = [
-  '/',
-];
+const urlsToCache = ["/"];
 
 // Install a service worker
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   // Perform install steps
-  self.skipWaiting()
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 // Cache and return requests
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
       }
-    )
+      return fetch(event.request);
+    })
   );
 });
 
 // Update a service worker
-self.addEventListener('activate', event => {
-  const cacheWhitelist = ["pwa-v4"]
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = ["pwa-v5"];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
