b08e87d1579eeff875e55f1b6ef616193cf5b230
Aditya Shekhar <aditya.shekhar9@byjus.com>
2022-12-24 19:35:28 +0530

    scroll to Top on Tab change - Resume

--- src/components/Home/NewResume.js
+++ src/components/Home/NewResume.js
@@ -1,20 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import cx from "classnames";
 import About from "./About";
 import Experience from "./Experience";
 import Skills from "./Skills";
+import useWindowResize from "../../utils/hooks/useWindowResize";
 import { TABS, ABOUT, EXPERIENCE, SKILLS } from "./resume.constants";
 
 import styles from "./NewResume.module.scss";
 
 export default function Resume() {
   const [activeTab, setActiveTab] = useState(ABOUT);
+  const [screenWidth] = useWindowResize();
 
   const renderTabContent = () => {
     if (activeTab === ABOUT) return <About />;
     if (activeTab === EXPERIENCE) return <Experience />;
     if (activeTab === SKILLS) return <Skills />;
   };
+
+  useEffect(() => {
+    if (activeTab && screenWidth < 520) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  }, [activeTab, screenWidth]);
   return (
     <div className={styles.wrapper}>
       <div className={styles.tabs}>
