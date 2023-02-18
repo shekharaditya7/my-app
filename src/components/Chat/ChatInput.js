724d778ad0c5f50318613dc7bc613a689a455ac0
Aditya Shekhar <aditya.shekhar9@byjus.com>
2023-02-18 16:27:38 +0530

    Chat Layout Changes - max width

--- src/components/Chat/ChatInput.js
+++ src/components/Chat/ChatInput.js
@@ -1,20 +1,25 @@
 import { useState } from "react";
 import cx from "classnames";
+import useKeyPress from "../../utils/hooks/useKeyPress";
 import styles from "./ChatInput.module.scss";
 
 export default function ChatInput({ handleSendMessage, className }) {
   const [textMessage, setTextMessage] = useState("");
-
+  const sendButtonClick = () => {
+    handleSendMessage(textMessage);
+    setTextMessage("");
+  };
+  useKeyPress({ keyCode: 13, onKeyPress: sendButtonClick });
   return (
     <div className={cx(styles.wrapper, className)}>
       <div className={styles.header}>
         <input
-          type="url"
+          type="text"
           onChange={(event) => setTextMessage(event.target.value)}
           value={textMessage}
           className={styles.urlInput}
         ></input>
-        <button onClick={() => handleSendMessage(textMessage)}>Send</button>
+        <button onClick={sendButtonClick}>Send</button>
       </div>
     </div>
   );
